c32f7b85d823795585ef35e0da4312b397bf4006
himanshuc11 <himanshuchhatpar@gmail.com>
2022-01-20 19:55:57 +0530

    Ebay2

---
+++ customUser/views.py
@@ -0,0 +1,20 @@
+import imp
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def user_login(request):
+    return HttpResponse('LOGIN PAGE')
+
+def user_login(request):
+    return HttpResponse('LOGIN PAGE')
+
+def user_logout(request):
+    return HttpResponse('LOGOUT PAGE')
+
+def user_register(request):
+    return HttpResponse('REGISTER PAGE')
+
+    
+
+    

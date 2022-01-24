0f5ab26bd41c7bfe72fd36a9f5c65ff9780b6b62
himanshuc11 <himanshuchhatpar@gmail.com>
2022-01-24 20:03:02 +0530

    Login/Logout

---
+++ customUser/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from .models import User  
+
+from django.contrib.auth.forms import UserCreationForm  
+
+class UserLoginForm(forms.Form):
+    username = forms.CharField(max_length=128, widget=forms.TextInput(attrs={'class': "form-control"}))
+    password = forms.CharField(max_length=128, widget=forms.TextInput(attrs={'class': "form-control", 'type': "password"}))
+
+# class UserRegistrationForm(forms.ModelForm):
+#     class Meta:
+#         model = User
+#         fields = ['username', 'password']
+
+class UserRegistrationForm(UserCreationForm):
+    pass
--- customUser/views.py
+++ customUser/views.py
@@ -1,19 +1,44 @@
-import imp
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .forms import UserLoginForm, UserRegistrationForm
+
 # Create your views here.
-def user_login(request):
-    return HttpResponse('LOGIN PAGE')
+def user_register(request):
+    if request.method == "GET":
+        return render(request, 'customUser/register.html', {"form": UserRegistrationForm()})
+    if request.method == "POST":
+        # Make bounded form
+        # Check if valid
+        # Extract username and password
+        # create_user()
+        # return SUCCESS
+        pass
+
 
 def user_login(request):
-    return HttpResponse('LOGIN PAGE')
+    if request.method == "GET":
+        return render(request, 'customUser/login.html', {"form": UserLoginForm()})
+    if request.method == "POST":
+        bounded_user_login_form = UserLoginForm(request.POST)
+        if bounded_user_login_form.is_valid():
+            username = bounded_user_login_form.cleaned_data['username']
+            password = bounded_user_login_form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return HttpResponse('SUCCESS')
+            else:
+                return HttpResponse("Wrong username or password")
+        else:
+            return HttpResponse(bounded_user_login_form.errors)
+    
 
 def user_logout(request):
-    return HttpResponse('LOGOUT PAGE')
+    logout(request)
+    return HttpResponse('You have been logged out')
 
-def user_register(request):
-    return HttpResponse('REGISTER PAGE')
 
     
 
